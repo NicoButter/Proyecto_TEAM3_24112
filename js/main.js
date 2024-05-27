697dda0c261ecefe107f829e861a6444d04f469d
Nicolás Butterfield <nicobutter@gmail.com>
2024-05-27 10:09:09 -0300

    ajustes

--- js/main.js
+++ js/main.js
@@ -3,23 +3,19 @@ function cargarHeaderYFooter() {
     var rutaHeader = "./templates/header.html";
     var rutaFooter = "./templates/footer.html";
 
-    console.log("La ruta al header es: " + rutaHeader);
-    
-    console.log("La ruta al footer es: " + rutaFooter);
-
     // Cargar el header
     fetch(rutaHeader)
         .then(response => response.text())
         .then(data => {
             document.getElementById("header").innerHTML = data;
-    });
-    
+        });
+
     // Cargar el footer
     fetch(rutaFooter)
         .then(response => response.text())
         .then(data => {
             document.getElementById("footer").innerHTML = data;
-    });
-}    
+        });
+}
 
 cargarHeaderYFooter();
